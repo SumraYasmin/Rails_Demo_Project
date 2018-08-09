03fa6b574b098efdb46ced050d5713e35e6b8926
SumraYasmin <sumrayasmin9@gmail.com>
2018-08-09 19:33:51 +0500

    Implementation of bootstrap to all form and header is done

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,18 +6,4 @@ class User < ApplicationRecord
          
   enum role: [:user, :admin, :manager]
   enum status: [:active, :inactive]
-  after_initialize :set_default_role, :set_default_status,  :if => :new_record?
-
-  def set_default_role
-    self.role ||= :user
-  end
-
-  def set_default_status
-    self.status ||= :active
-  end
-
-   before_create do
-    self.first_name = first_name.capitalize
-    self.last_name = last_name.capitalize
-  end
 end
--- db/migrate/20180809064535_add_details_to_user.rb
+++ db/migrate/20180809064535_add_details_to_user.rb
@@ -1,9 +1,9 @@
 class AddDetailsToUser < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :first_name, :string
+    add_column :users, :first_name, :string 
     add_column :users, :last_name, :string
     add_column :users, :date_of_birth, :date
-    add_column :users, :role, :integer
-    add_column :users, :status, :integer
+    add_column :users, :role, :integer, default: 0
+    add_column :users, :status, :integer, default: 0
   end
 end
