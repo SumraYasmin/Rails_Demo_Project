0b1f80c9f4af268c4755235e513334e78698218b
SumraYasmin <sumrayasmin9@gmail.com>
2018-08-09 12:16:50 +0500

    User:Addition of customized attributes to devise is compeleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :first_name, :last_name, presence: true
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  enum role: [:user, :admin, :manager]
+  enum status: [:active, :inactive]
+  after_initialize :set_default_role, :set_default_status,  :if => :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
+
+  def set_default_status
+    self.status ||= :active
+  end
+
+   before_create do
+    self.first_name = first_name.capitalize
+    self.last_name = last_name.capitalize
+  end
 end
---
+++ db/migrate/20180809064535_add_details_to_user.rb
@@ -0,0 +1,9 @@
+class AddDetailsToUser < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :date_of_birth, :date
+    add_column :users, :role, :integer
+    add_column :users, :status, :integer
+  end
+end
